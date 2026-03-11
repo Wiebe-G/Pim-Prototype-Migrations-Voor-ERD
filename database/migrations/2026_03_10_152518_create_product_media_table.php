6f55aca5fb2a77c8316cbcd47ccee68ac223ddee
Wiebe Gouma <goumawiebe1234@proton.me>
2026-03-11 09:41:24 +0100

    product media, product stock, category, en sync migrations

---
+++ database/migrations/2026_03_10_152518_create_product_media_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_media', function (Blueprint $table) {
+            // aparte entry voor elke foto of video (maak het in de controller zo dat het foto en ook bijv. mp4 accepteert
+            $table->id();
+            $table->foreignId('product_id')->constrained('product', 'id')->cascadeOnDelete();
+            $table->string('path')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_media');
+    }
+};
---
+++ database/migrations/2026_03_11_080148_create_sync_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sync_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('run_id')->constrained('sync_runs', 'id')->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained('product', 'id')->cascadeOnDelete();
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sync_items');
+    }
+};
